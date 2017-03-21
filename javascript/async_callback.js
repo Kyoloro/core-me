bb210acd4ab1aded744ad0a05c333c145328b600
李鹏程 <379623292@qq.com>
2017-03-21 23:55:52 +0800

    async_callback_translate

---
+++ javascript/async_callback.js
@@ -0,0 +1,41 @@
+// 闭包 偏函数
+const _async = (fns) => {
+    let count = 0
+    let cbAll
+    let arr = []
+
+    fns.forEach((fn, i) => {
+        fn((err, result) => {
+            ++count
+            arr[i] = result
+            if (count === fns.length) {
+                cbAll(arr)
+            }
+        })
+    })
+
+    return {
+        then(cb) {
+            cbAll = cb
+        }
+    }
+}
+
+// Async Function
+const fn = (cb) => {
+    setTimeout(() => {
+        cb(null, 1)
+    }, 1000)
+}
+
+// 封装 实现效果
+console.time()
+_async([
+    fn,
+    fn
+]).then((arr) => {
+    console.timeEnd() // 1002.782ms
+    console.log(arr) // [1, 1]
+})
+
+console.log('无同步阻塞')
